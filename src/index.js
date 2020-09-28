c79cc6a871dbb5c6d9a4dfd3f8f156750d4793bd
Matt <matthave@gmail.com>
2020-09-28 23:50:11 +0200

    Create nav element, create trip element, add style for nav and trip element, add mixins

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./SASS/style.scss";
+
+class App {
+  constructor() {
+    this.burgerToggle = false;
+  }
+
+  init = () => {
+    const burgerIcon = document.querySelector(".burger__container");
+    burgerIcon.addEventListener("click", () => this.burgerClickFunc());
+  };
+
+  burgerClickFunc = () => {
+    const ulElement = document.querySelector(".nav__ul");
+    if (this.burgerToggle) {
+      ulElement.classList.remove("nav__ul--hideToggle");
+    } else {
+      ulElement.classList.add("nav__ul--hideToggle");
+    }
+    this.burgerToggle = !this.burgerToggle;
+  };
+}
+
+const app = new App();
+
+app.init();
