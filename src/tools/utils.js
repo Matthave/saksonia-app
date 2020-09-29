9ef27ab063c111b2c7bc1bf2ccb25049f7c4b3ac
Matt <matthave@gmail.com>
2020-09-29 18:27:27 +0200

    Fix slider style issue, fix dots style issue

--- src/tools/utils.js
+++ src/tools/utils.js
@@ -2,10 +2,13 @@ export const dotsFilterTool = (e, listColor) => {
   const classListArray = [...e.target.classList];
   const allDots = document.querySelectorAll(".dot");
   const currentDots = document.querySelectorAll(`.dot__${classListArray[1]}`);
-  allDots.forEach((ele) => (ele.style.display = "none"));
-  currentDots.forEach((ele) => (ele.style.display = "initial"));
+  allDots.forEach((ele) => (ele.style.opacity = "0"));
+  allDots.forEach((ele) => (ele.style.pointerEvents = "none"));
+  currentDots.forEach((ele) => (ele.style.opacity = "1"));
+  currentDots.forEach((ele) => (ele.style.pointerEvents = "initial"));
   if (classListArray[1] === "all") {
-    allDots.forEach((ele) => (ele.style.display = "initial"));
+    allDots.forEach((ele) => (ele.style.opacity = "1"));
+    allDots.forEach((ele) => (ele.style.pointerEvents = "initial"));
   }
 
   const tripLists = document.querySelectorAll(".trip__list");
